bdd08d1281da2a1f875cedd19a99cebc4c4c4b43
HardLaif <suchhardlaif@vk.com>
2024-02-14 19:36:53 +0300

    feat: Шаблонизируй то

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -1,45 +1,32 @@
 import NewEditPoint from '../view/edit-point.js';
 import NewListItem from '../view/list-item.js';
-import NewTripEvents from '../view/trip-events.js';
 import {render, replace,} from '../framework/render.js';
 
 export default class PointPresenter {
-  #tripEvents = new NewTripEvents();
   #listItem = null;
   #editPoint = null;
   #tripContainer = null;
   #tripModel = null;
-  #tripPresenter = null;
-  constructor({tripContainer, tripModel, tripPresenter}) {
-    this.#tripContainer = tripContainer;
-    this.#tripModel = tripModel;
-    this.#tripPresenter = tripPresenter;
+  #tripEvents = null;
+  constructor({pointData, tripEvents}) {
+    this.pointData = pointData;
+    this.#tripEvents = tripEvents;
+    // this.#tripContainer = tripContainer;
+    // this.#tripModel = tripModel;
   }
 
   init() {
-    render(this.#tripEvents, this.#tripContainer);
-
-
-    for (let i = 0; i < this.#tripModel.trips.length; i++) {
-      this.#renderTrip(this.#tripModel.trips[i]);
-    }
+    this.#renderTrip(this.pointData);
   }
 
   #replaceItemToForm() {
     replace(this.#editPoint, this.#listItem);
   }
 
-  #replaceFormToItem(){
+  replaceFormToItem(){
     replace(this.#listItem, this.#editPoint);
   }
 
-  #escKeyDownHandler(evt){
-    if (evt.key === 'Escape') {
-      evt.preventDefault();
-      this.replaceFormToItem();
-      document.removeEventListener('keydown', this.#escKeyDownHandler);
-    }
-  }
 
   #renderTrip(trip){
 
@@ -47,20 +34,16 @@ export default class PointPresenter {
       trip,
       onEditClick: () => {
         this.#replaceItemToForm();
-        document.addEventListener('keydown', this.#escKeyDownHandler);
       }});
     this.#editPoint = new NewEditPoint({
       onFormSubmit: () => {
-        this.#replaceFormToItem();
-        document.removeEventListener('keydown', this.#escKeyDownHandler);
+        this.replaceFormToItem();
       },
-
       onFormClose: () => {
-        this.#replaceFormToItem();
+        this.replaceFormToItem();
       }
 
     });
-
 
     render (this.#listItem, this.#tripEvents.element);
   }
--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,35 +1,55 @@
 import PointPresenter from './point-presenter.js';
-import NewTripEvents from '../view/trip-events.js';
 import NewListEmpty from '../view/list-empty.js';
+import NewTripEvents from '../view/trip-events.js';
 import { render } from '../framework/render.js';
 
 
 export default class TripPresenter {
   #tripEvents = new NewTripEvents();
-  #listItem = null;
-  #editPoint = null;
-  #tripContainer = null;
   #tripModel = null;
-  #pointPresenter = null;
+  #pointPresenters = [];
   #tripList = [];
+  #tripContainer = null;
 
 
   constructor({tripContainer, tripModel}) {
     this.#tripContainer = tripContainer;
     this.#tripModel = tripModel;
-    this.#pointPresenter = new PointPresenter({tripContainer: this.#tripContainer, tripModel: this.#tripModel, tripPresenter: this});
+    //this.#pointPresenter = new PointPresenter({pointData});
+  }
 
-    this.init();
+  #renderList(){
+    for (let i = 0; i < this.#tripModel.trips.length; i++) {
+      const pointData = this.#tripModel.trips[i];
+      const pointPresenter = new PointPresenter({
+        pointData,
+        tripEvents: this.#tripEvents,
+      });
+      pointPresenter.init();
+      this.#pointPresenters.push(pointPresenter);
+    }
   }
 
   init() {
     this.#tripList = [...this.#tripModel.trips];
     this.#renderEmptyList();
+    render(this.#tripEvents, this.#tripContainer);
+    this.#renderList();
+    document.addEventListener('keydown', this.#escKeyDownHandler);
   }
+
+  #escKeyDownHandler = (evt) => {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      for (let i = 0; i < this.#pointPresenters.length; i++) {
+        this.#pointPresenters[i].replaceFormToItem();
+      }
+    }
+  };
 
   #renderEmptyList() {
     if (this.#tripList.length === 0) {
-      render(new NewListEmpty(), this.#tripList.element);
+      render(new NewListEmpty(), this.#tripEvents.element);
     }
   }
 
